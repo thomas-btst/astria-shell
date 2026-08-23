02e1ec7cefb98c9d335fb9b145812d369ef4bae4
Thomas BATISTA <85026445+thomas-btst@users.noreply.github.com>
2026-08-23 12:02:43 +0000

    fix(wifi): handle null wifi state safely in WifiTray

--- windows/bar/modules/trays/wifi/WifiTray.tsx
+++ windows/bar/modules/trays/wifi/WifiTray.tsx
@@ -1,4 +1,4 @@
-import { createBinding, createComputed } from "ags"
+import { createBinding, createComputed, createState } from "ags"
 import AstalNetwork from "gi://AstalNetwork?version=0.1"
 import { Cursor } from "../../../../../utils/gtk"
 import { execAsync } from "ags/process"
@@ -10,17 +10,27 @@ const network = AstalNetwork.get_default()
 
 const wifi = createBinding(network, "wifi")
 
-const internet = Utils.unnestBinding(wifi((wifi) => createBinding(wifi, "internet")))
-const ssid = Utils.unnestBinding(wifi((wifi) => createBinding(wifi, "ssid")))
-const strength = Utils.unnestBinding(wifi((wifi) => createBinding(wifi, "strength")))
+const internet = Utils.unnestBinding(
+    wifi((wifi) =>
+        wifi === null ? createState(AstalNetwork.Internet.DISCONNECTED)[0] : createBinding(wifi, "internet"),
+    ),
+)
+const ssid = Utils.unnestBinding(wifi((wifi) => (wifi === null ? createState("")[0] : createBinding(wifi, "ssid"))))
+const strength = Utils.unnestBinding(
+    wifi((wifi) => (wifi === null ? createState(0)[0] : createBinding(wifi, "strength"))),
+)
 
 const networkingEnabled = Utils.unnestBinding(
     createBinding(network, "client")((client) => createBinding(client, "networkingEnabled")),
 )
 
-const iconName = Utils.unnestBinding(wifi((wifi) => createBinding(wifi, "iconName")))
+const iconName = Utils.unnestBinding(
+    wifi((wifi) => (wifi === null ? createState("")[0] : createBinding(wifi, "iconName"))),
+)
 
-const wifiEnabled = Utils.unnestBinding(wifi((wifi) => createBinding(wifi, "enabled")))
+const wifiEnabled = Utils.unnestBinding(
+    wifi((wifi) => (wifi === null ? createState(false)[0] : createBinding(wifi, "enabled"))),
+)
 
 const isWifi = createComputed((get) => get(networkingEnabled) && get(wifiEnabled))
 
@@ -49,7 +59,14 @@ export const WifiTray: TrayItem = {
                         execAsync("networkmanager_dmenu").catch(console.error)
                     }}
                 />
-                <Gtk.GestureClick button={3} onPressed={() => (network.wifi.enabled = !network.wifi.enabled)} />
+                <Gtk.GestureClick
+                    button={3}
+                    onPressed={() => {
+                        if (network.wifi) {
+                            network.wifi.enabled = !network.wifi.enabled
+                        }
+                    }}
+                />
             </box>
         )
     },
